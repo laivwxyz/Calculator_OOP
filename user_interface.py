5d74ef2f123439465dc446c6ef8006171efdc678
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-22 22:40:51 +0800

    Changes Occur

--- user_interface.py
+++ user_interface.py
@@ -2,8 +2,7 @@
 class UserInterface:
     # Display mathematical operation
     def operation(self):
-        print("Calculator")
-        print("Choose one mathemathical operation: ")
+        print("Choose one mathematical operation: ")
         print("1 : Addition")
         print("2 : Subtraction")
         print("3 : Multiplication ")
