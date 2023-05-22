7abafdca7eee0f41263442e74c3725295e48cb4a
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-22 15:42:00 +0800

    Display the mathematical operation

--- user_interface.py
+++ user_interface.py
@@ -1,3 +1,10 @@
 # Creating a class named UserInterface
 class UserInterface:
-    
+    # Display the mathematical operation
+    def operation(self):
+        print("Calculator")
+        print("Choose one mathemathical operation: ")
+        print("1 : Addition")
+        print("2 : Subtraction")
+        print("3 : Multiplication ")
+        print("4 : Division")
