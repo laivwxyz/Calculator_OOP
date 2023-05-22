57e61ae4dea9d560cf8f733abea5d6646abc5cc9
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-22 15:57:08 +0800

    If user choose addition

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 # Creating a class named Calculator
 class MathematicalOperator:
-    
+# Performing the chosen operation
+    # Addition,
+    def addition(self, num1, num2):
+        # if the user choose addition,
+        result = num1 + num2
+        return result
