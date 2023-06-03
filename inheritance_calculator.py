da4dc91b09173393df57adc2996a53709cb6ae9e
laivwxyz <delacruzlailanie2004@gmail.com>
2023-06-03 16:46:53 +0800

    Adding some design

--- inheritance_calculator.py
+++ inheritance_calculator.py
@@ -5,11 +5,15 @@ class CalculatorInheritance(Calculator):
     # Modulus,
     def modulus(self, num1, num2):
         # if the user choose modulus,
+        print()
+        print("\033[96m>" * 5, "\033[0m The mathematical operation you choose is \033[91mModulus")
         result = num1 % num2
         return result
 
     # Exponent,
     def exponent(self, num1, num2):
-        # if the user choose modulus,
+        # if the user choose exponent,
+        print()
+        print("\033[96m>" * 5, "\033[0m The mathematical operation you choose is \033[92mExponent")
         result = num1 ** num2
         return result
