253b88a984da63108a0a080ed8c242f8f6f950ce
laivwxyz <delacruzlailanie2004@gmail.com>
2023-06-03 16:28:50 +0800

    adding new operation to the program

--- inheritance_calculator.py
+++ inheritance_calculator.py
@@ -2,3 +2,8 @@ from calculator import Calculator
 
 # Creating a class named CalculatorInheritance
 class CalculatorInheritance(Calculator):
+    # Modulus,
+    def modulus(self, num1, num2):
+        # if the user choose modulus,
+        result = num1 % num2
+        return result
